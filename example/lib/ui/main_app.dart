eef2db8aa897597a2fb3a5cdafc4212a0c6f75cf
Temirlan <zatovagul@gmail.com>
2022-10-13 12:33:51 +0300

    initial

---
+++ example/lib/ui/main_app.dart
@@ -0,0 +1,13 @@
+import 'package:example/ui/pages/main_page.dart';
+import 'package:flutter/material.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
